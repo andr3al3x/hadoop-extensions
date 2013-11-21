75db82884c5e6d7cbf3745c5feaa5176b8009bfa
André Simões <alexandr3@gmail.com>
2013-11-21 17:06:27 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[![githalytics.com alpha](https://cruel-carlota.pagodabox.com/431247ad1c7ddc6490b40d1cab7c2943 "githalytics.com")](http://githalytics.com/ITXpander/hadoop-extensions)
+
+hadoop-extensions
+=================
+
+A project with random hadoop extensions
---
+++ src/main/java/org/pentaho/hadoop/mapred/KeyBasedMultipleTextOutputFormat.java
@@ -0,0 +1,38 @@
+package org.pentaho.hadoop.mapred;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
+
+/**
+ * Outputs multiple text files based on the key path
+ * NOTE: THIS IS NOT PRODUCTION READY AND IT MUST BE TESTED
+ */
+public class KeyBasedMultipleTextOutputFormat extends MultipleTextOutputFormat<Text, Text> {
+
+    /**
+     * Overriden method that returns the file name based on the key value
+     *
+     * @param key
+     * @param value
+     * @param name
+     * @return
+     */
+    @Override
+    protected String generateFileNameForKeyValue(Text key, Text value, String name) {
+        return new Path(key.toString(), name).toString();
+    }
+
+
+    /**
+     * Overriden method used to discard the key as its being used in the path already
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    @Override
+    protected Text generateActualKey(Text key, Text value) {
+        return null;
+    }
+}
